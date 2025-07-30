cb0547a721bbdfcb7afba02d59f481690587987c
Le Ying <24009849@myrp.edu.sg>
2025-07-30 22:35:30 +0800

    update

--- app.js
+++ app.js
@@ -775,7 +775,7 @@ app.get('/dessertsList', (req, res) => {
 
   db.query(query, [user?.id || 0], (err, results) => {
     if (err) throw err;
-    res.render('sdessertsList', { recipes: results, user });
+    res.render('dessertsList', { recipes: results, user });
   });
 });
 
@@ -808,7 +808,7 @@ app.get('/breakfastList', (req, res) => {
 
   db.query(query, [user?.id || 0], (err, results) => {
     if (err) throw err;
-    res.render('beakfastList', { recipes: results, user });
+    res.render('breakfastList', { recipes: results, user });
   });
 });
 
